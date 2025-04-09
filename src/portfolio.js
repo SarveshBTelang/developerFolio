2a34337626c6f52163e050c546a8fc4582fead67
SARVESH TELANG <telang.sarvesh98@gmail.com>
2025-04-09 17:13:18 +0200

    ver1.0 mod

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,26 +20,22 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Sarvesh Telang",
+  title: "Hi all, I'm Sarvesh..",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "Im a passionate and detail-oriented engineer with around 3 years of experience in the automotive industry, specializing in data-driven development, machine learning, and software solutions for intelligent mobility. I focus on building scalable ML pipelines, computer vision tools, and automation frameworks that are robust, maintainable, and production-ready."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1aAsgfR4Hb0pUoqNYpTSCVuS9g5Fr3CLH/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/SarveshBTelang",
+  linkedin: "https://www.linkedin.com/in/sarvesh-telang-17916448/",
+  gmail: "telang@rptu.de",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -49,7 +45,7 @@ const socialMediaLinks = {
 
 const skillsSection = {
   title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  subTitle: "EXPERIENCED SOFTWARE DEVELOPER | DATA SCIENTIST",
   skills: [
     emoji(
       "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
