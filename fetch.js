e6a740f0df5b79f9755e1bdfcdf196049ccec7c0
SARVESH TELANG <telang.sarvesh98@gmail.com>
2025-04-15 03:35:16 +0200

    ver 1.7 git tocken update, index.html fix

--- fetch.js
+++ fetch.js
@@ -72,6 +72,7 @@ if (USE_GITHUB_DATA === "true") {
 
     console.log(`statusCode: ${res.statusCode}`);
     if (res.statusCode !== 200) {
+      console.log(res.statusCode);
       throw new Error(ERR.requestFailed);
     }
 
